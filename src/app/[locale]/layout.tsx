ce23f1d0421ce33be7cc05f4d6e236fa5d8de58e
HolyMolly <104388974+hHolyMolly@users.noreply.github.com>
2025-12-23 01:30:03 +0200

    Refactor LocaleLayout to use ClientProviders component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,15 +7,19 @@ import { getBaseMetadata } from '@configs/metadata';
 
 import { routing } from '@services/i18n/routing';
 
-import ClientProvidersProps from '@components/layouts/ClientProviders';
+import ClientProviders from '@components/layouts/ClientProviders';
 import Header from '@components/layouts/Header';
-import Footer from '@components/layouts/Footer';
+
+type LocaleLayoutProps = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
 
 export async function generateMetadata(): Promise<Metadata> {
   return getBaseMetadata();
 }
 
-async function LocaleLayout({ children, params }: LayoutProps<'/[locale]'>) {
+async function LocaleLayout({ children, params }: LocaleLayoutProps) {
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
@@ -24,15 +28,13 @@ async function LocaleLayout({ children, params }: LayoutProps<'/[locale]'>) {
 
   return (
     <NextIntlClientProvider locale={locale}>
-      <ClientProvidersProps>
+      <ClientProviders>
         <div className="wrapper">
           <Header />
 
           <main className="page">{children}</main>
-
-          <Footer />
         </div>
-      </ClientProvidersProps>
+      </ClientProviders>
     </NextIntlClientProvider>
   );
 }
